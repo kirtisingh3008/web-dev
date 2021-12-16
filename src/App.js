523c474f77dd205e17bd80dba09e0430f7b6b099
kirtisingh3008 <kirtisingh3008@gmail.com>
2021-12-16 21:33:16 +0530

    Adding Pagination Codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import react, {useState} from 'react';
+import Paginate from 'react-paginate';
+import fakedata from "./Fake_data.json"
 import './App.css';
 
+// let's make a funtion for diaplaying data
 function App() {
+
+  const [data, setData] =useState(fakedata.slice(0,40));
+  const [pageNumber, setPageNumber] =useState(0);
+  const dataPerPage =6;
+  const pageVisited = pageNumber * dataPerPage;
+
+  // making function for fetching data
+  const fetchData = data.slice(pageVisited, pageVisited + dataPerPage).map((x)=>{
+    return(
+      <div className="card">
+        <h3>{x.FullName}</h3>
+        <h3>{x.CompanyName}</h3>
+        <h3>{x.URL}</h3>
+        </div>
+    );
+  })
+// we are using ceil function here because if it not proper divisible value then we need an extra page for
+// the remainder
+const pageCount = Math.ceil(data.length/dataPerPage)
+
+// function for page change set the page we are currently on
+const changePage = ({selected}) => {
+  setPageNumber(selected);
+}
   return (
+    <>
+     <div className="header"> <h2>WELCOME TO FAKEDATA WORLD</h2></div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {fetchData}
+      <Paginate
+      previousLabel ={"Prev"}
+      afterLabel ={"After"}
+      pageCount ={pageCount}
+      onPageChange={changePage}
+      containerClassName={"paginationBttns"}
+      previousLinkClassName={"previousBttn"}
+      nextLinkClassName={"nextBttn"}
+      disabledClassName={"paginationDisabled"}
+      activeClassName={"paginationActive"}
+
+      />
     </div>
+    </>
   );
 }
 
